cd48308a640c38d98d6de303c83651f2da9f35f0
Romalvir Singh <romiie91@gmail.com>
2020-12-14 01:22:59 -0500

    DOM

---
+++ script.js
@@ -0,0 +1,8 @@
+//there is a problem in the game where bigfoot loads before the background image.
+//make a function to fix that
+
+function loadBigFoot() {
+	document.getElementById("bigFoot").src = "bigfoot.png"
+}
+
+//make game harder! move big foot around
